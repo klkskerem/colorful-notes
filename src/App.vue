c507af897a245ff2b4782076ba15fa6ef61a4343
klkskerem <keremklks@gmail.com>
2024-12-18 16:53:37 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,159 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <div v-if="showModal" class="overlay">
+      <div class="modal">
+        <textarea v-model.trim="newNote" name="note" id="note" cols="30" rows="10"></textarea>
+        <p v-if="errorMessage">{{errorMessage}}</p>
+        <button @click="addNote">Not Ekle</button>
+        <button class="close" @click="showModal = false">Kapat</button>
+      </div>
+    </div>
+    <div class="container">
+      <header>
+        <h1>Notlar</h1>
+        <button @click="showModal = true">+</button>
+      </header>
+      <div class="cards-container">
+        <div 
+          v-for="note in notes"
+          :key="note.id"
+          class="card" 
+          :style="{backgroundColor: note.backgroundColor}"
+        >
+          <p class="main-text">{{ note.text }}</p>
+          <p class="date">{{ note.date.toLocaleDateString("tr") }}</p>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+  import {ref} from "vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  const showModal = ref(false)
+  const newNote = ref("");
+  const errorMessage = ref("")
+  const notes = ref([]);
+
+  function getRandomColor() {
+    return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
   }
-}
+
+  const addNote = () => {
+    if(newNote.value.length < 10) {
+      return errorMessage.value = "Not eklemek için en az 10 karakter girin"
+    }
+    notes.value.push({
+      id: Math.floor(Math.random() * 1000000),
+      text: newNote.value,
+      date: new Date(),
+      backgroundColor: getRandomColor()
+    });
+    showModal.value = false;
+    newNote.value = ""
+    errorMessage.value = ""
+  }
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  main {
+    height: 100vh;
+    width: 100vw
+  }
+
+  .container {
+    max-width: 1000px;
+    padding: 10px;
+    margin: 0 auto
+  }
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  h1 {
+    font-weight: bold;
+    margin-bottom: 25px;
+    font-size: 75px;
+  }
+
+  header button {
+    border: none;
+    padding: 10px;
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+    background-color: rgb(21,20,20);
+    border-radius: 100%;
+    color: white;
+    font-size: 20px;
+  }
+
+  .card {
+    width: 225px;
+    height: 225px;
+    background-color: rgb(237, 182, 44);
+    padding: 10px;
+    border-radius: 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-right: 20px;
+    margin-bottom: 20px;
+  }
+
+  .date {
+    font-size: 12.5px;
+    font-weight: bold;
+  }
+
+  .cards-container {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .overlay {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,0.77);
+    z-index: 10;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .modal {
+    width: 750px;
+    background-color: white;
+    border-radius: 10px;
+    padding: 30px;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .modal button {
+    padding: 10px 20px;
+    font-size: 20px;
+    width: 100%;
+    background-color: blueviolet;
+    border: none;
+    color: white;
+    cursor: pointer;
+    margin-top: 15px
+  }
+
+  .modal .close {
+    background-color: rgb(193, 15, 15);
+    margin-top: 7px;
+  }
+
+  .modal p {
+    color: rgb(193, 15, 15);
+  }
 </style>
